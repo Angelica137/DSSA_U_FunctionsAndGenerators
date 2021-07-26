87337e82d4494da83d4aaab7a9158856cf9a0431
Angelica <Angelica137@users.noreply.github.com>
2021-07-26 16:48:44 +0100

    test fact_gen yields 2! correctly, red

--- tests/test_exercise1.py
+++ tests/test_exercise1.py
@@ -4,3 +4,21 @@ from scripts.exercise1 import fact_gen
 
 def test_prod_returns_product_of_a_and_b():
     assert prod(8, 5) == 40
+
+
+def test_fact_gen_returns_1factorial():
+    my_gen = fact_gen()
+    num = 1
+    result = 0
+    for i in range(num):
+        result = next(my_gen)
+    assert result == 1
+
+
+def test_fact_gen_returns_2factorial():
+    my_gen = fact_gen()
+    num = 2
+    result = 0
+    for i in range(num):
+        result = next(my_gen)
+    assert result == 2
